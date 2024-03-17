b321069481295a0a1e10299a9488c20d224a4ece
tw1xxxxx <133611442+tw1xxxxx@users.noreply.github.com>
2024-03-18 02:23:22 +0300

    trbtr
    rtg4t

---
+++ PracticeDataset/PracticeDataset/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PracticeDataset.storeP8DataSetTableAdapters;
+using System;
+using System.Windows;
+namespace PracticeDataset
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProductsTableAdapter products = new ProductsTableAdapter();
+        OrderArchiveTableAdapter orders = new OrderArchiveTableAdapter();
+        CategoriesTableAdapter categories= new  CategoriesTableAdapter();
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            dataGrid1.ItemsSource = products.GetData();
+            dataGrid2.ItemsSource = orders.GetData();
+            dataGrid3.ItemsSource = categories.GetData();
+            //dataGrid.ItemsSource = products.GetData();
+        }
+
+    }
+}
